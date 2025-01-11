1b6e9cca2f8895680bd38123c61689f9b36edd85
mohamed-tahiri <medtohairi@gmail.com>
2025-01-11 09:56:32 +0100

    initial mysql connection

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -18,8 +18,10 @@ repositories {
 }
 
 dependencies {
-	//implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	runtimeOnly("com.mysql:mysql-connector-j")
+
 }
 
 tasks.withType<Test> {
